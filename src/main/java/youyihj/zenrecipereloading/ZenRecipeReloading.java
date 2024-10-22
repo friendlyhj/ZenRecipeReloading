2c56f8736fb873b3f67200ec713c055a6a3e85af
friendlyhj <youyi580@qq.com>
2024-10-22 21:33:42 +0800

    v1.4.4

--- src/main/java/youyihj/zenrecipereloading/ZenRecipeReloading.java
+++ src/main/java/youyihj/zenrecipereloading/ZenRecipeReloading.java
@@ -43,7 +43,7 @@ public class ZenRecipeReloading {
 
     public static final String MOD_ID = "zenrecipereloading";
     public static final String MOD_NAME = "ZenRecipeReloading";
-    public static final String VERSION = "1.4.3";
+    public static final String VERSION = "1.4.4";
     public static final String DEPENDENCIES = "required-after:zenutils@[1.16.4,);required-after:jei;required-after:modtweaker";
 
     /**
